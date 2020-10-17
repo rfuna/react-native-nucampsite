74ded7064ae7eadd019e12160b391700880dbaf8
Ray <rayfunahashi@gmail.com>
2020-10-17 11:27:03 -0500

    week 1 workshop

---
+++ components/ContactComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { Card } from 'react-native-elements';
+import { View, Text, ScrollView } from 'react-native';
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  static navigationOptions = {
+    title: 'Contact Us'
+}
+
+  render() {
+    return (
+    <ScrollView>
+      <Card
+        title='Contact Information'
+        wrapperStyle={{margin: 10}}
+      >
+        <Text>
+        1 Nucamp Way
+        </Text>
+        <Text
+          style={{marginBottom: 10}}>
+        Seattle, WA 98001 U.S.A. 
+        </Text>
+        <Text>Phone: 1-206-555-1234 
+        </Text>
+        <Text>Email: campsites@nucamp.co</Text>
+      </Card>
+    </ScrollView>)
+    
+  };
+
+}
+
+export default Contact;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -11,6 +13,7 @@ const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
+
     },
     {
         initialRouteName: 'Directory',
@@ -43,10 +46,48 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const AboutNavigator = createStackNavigator(
+  {
+      About: { screen: About }
+  },
+  {
+      defaultNavigationOptions: {
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          }
+      }
+  }
+);
+
+const ContactNavigator = createStackNavigator(
+  {
+      Contact: { screen: Contact }
+  },
+  {
+      defaultNavigationOptions: {
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          }
+      }
+  }
+);
+
+// Drawer navigation
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        About: { screen: AboutNavigator },
+        Directory: { screen: DirectoryNavigator },
+        Contact: { screen: ContactNavigator },
     },
     {
         drawerBackgroundColor: '#CEC8FF'

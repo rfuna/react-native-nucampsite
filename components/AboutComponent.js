f0f6c2e6a59f36186cfa65ede04d7a072f733bb5
Ray <rayfunahashi@gmail.com>
2020-10-21 18:02:22 -0500

    Icons, Favorites, and Comments

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import { FlatList } from 'react-native';
-import { View, Text, ScrollView } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { Card } from 'react-native-elements';
-import { PARTNERS } from '../shared/partners';
+import React, { Component } from "react";
+import { FlatList } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { ListItem } from "react-native-elements";
+import { Card } from "react-native-elements";
+import { PARTNERS } from "../shared/partners";
 
 const Mission = () => {
   return (
-  <Card
-    title='Our Mission'>
-    <Text>
-    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-    </Text>
-  </Card>
+    <Card title="Our Mission">
+      <Text>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
   );
-}
+};
 
 class About extends Component {
   constructor(props) {
@@ -25,37 +30,33 @@ class About extends Component {
   }
 
   static navigationOptions = {
-    title: 'About Us'
-  }
+    title: "About Us",
+  };
 
   render() {
-    const renderPartner = ({item}) => {
+    const renderPartner = ({ item }) => {
       return (
-        
         <ListItem
-              title={item.name}
-              subtitle={item.description}
-              leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
-          />
-      )
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: require("./images/bootstrap-logo.png") }}
+        />
+      );
     };
 
     return (
-    <ScrollView>
-      <Mission />
-      <Card
-        title='Community Parners'>
-      <FlatList
-                data={this.state.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-            />
-      </Card>
-
-    </ScrollView>
-    )
-  };
-
+      <ScrollView>
+        <Mission />
+        <Card title="Community Parners">
+          <FlatList
+            data={this.state.partners}
+            renderItem={renderPartner}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        </Card>
+      </ScrollView>
+    );
+  }
 }
 
 export default About;
